bb3d95e5496b2c584f4bd97129cf335e09ff2ad0
Alina <12340987@rambler.ru>
2019-07-29 21:30:39 +0300

    Fixed errors

--- Numbers.py
+++ Numbers.py
@@ -9,10 +9,10 @@ today3 = 2019
 
 ok = today3 - year
 if today2 < month:
-    ok = ok + 1
+    ok = ok - 1
 else:
-    if month == today2 and today1 < day:
-        ok = ok + 1
+    if month == today2 and today1 <= day:
+        ok = ok - 1
 
 
 print("You age is ", ok)
