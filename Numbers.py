90b3435a139488eaa320e0bc5631a3bd1ed9bc0b
Alina <12340987@rambler.ru>
2019-07-29 19:52:52 +0300

    first commit

---
+++ Numbers.py
@@ -0,0 +1,18 @@
+day = int(input())
+month = int(input())
+year = int(input())
+
+
+today1 = 25
+today2 = 7
+today3 = 2019
+
+ok = today3 - year
+if today2 < month:
+    ok = ok + 1
+else:
+    if month == today2 and today1 < day:
+        ok = ok + 1
+
+
+print("You age is ", ok)
